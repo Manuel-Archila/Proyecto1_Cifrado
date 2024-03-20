202ec4ee2c368716a8968e8a3019a1474cdf1e0f
andresdlRoca <andresdelaroca11@gmail.com>
2024-03-19 20:57:05 -0600

    Register and Login screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Button, Divider, Form, Input, message } from 'antd';
+
+
+const onFinish = (values) => {
+  message.success('Success');
+  console.log('Success:', values);
+};
+
+const onFinishFailed = (errorInfo) => {
+  message.error('Failed');
+  console.log('Failed:', errorInfo);
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='app-grouped-forms'>
+        <Form
+          className='app-form'
+          name='register'
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          layout='horizontal'
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete='off'
         >
-          Learn React
-        </a>
-      </header>
+          <h1>Register</h1>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Enter your username' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Enter your password' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="Confirm Password"
+            name="confirmPassword"
+            dependencies={['password']}
+            rules={[
+              { required: true, message: 'Confirm your password' },
+              ({ getFieldValue }) => ({
+                validator(rule, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject('The two passwords do not match');
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+
+        </Form>
+        <Divider style={{ borderColor: 'black'}}></Divider>
+        <Form
+          className='app-form'
+          name='login'
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          layout='horizontal'
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete='off'
+        >
+          <h1>Login</h1>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Enter your username' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Enter your password' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+
+        </Form>
+      </div>
     </div>
   );
 }
